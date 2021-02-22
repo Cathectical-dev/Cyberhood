5896f5970aa299270535cd283ce5af4eacf5224c
Cathectical-dev <60582427+Cathectical-dev@users.noreply.github.com>
2021-02-22 19:45:51 +0100

    Laundry Simulator added to script

--- main/source.lua
+++ main/source.lua
@@ -2390,7 +2390,14 @@ FeaturedExploits = {
 		ImageID = 0,
 		Colour = Color3.fromRGB(71, 116, 170),
 		Code = "https://raw.githubusercontent.com/Cathectical-dev/Cyberhood/master/Scripthub/Tapping%20Masters.lua"
-	},	
+	},
+	laundrysim = {
+		Name = "Laundry Simulator",
+		Author = "Taka",
+		ImageID = 0,
+		Colour = Color3.fromRGB(71, 116, 170),
+		Code = "https://raw.githubusercontent.com/Cathectical-dev/Cyberhood/master/Scripthub/LaundrySimulator.lua"
+	}	
 
 }
 
